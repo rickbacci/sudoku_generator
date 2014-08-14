df7e9ff60e5af09972c86a501b03ae9a08aed353
Ricky Bacci <brownieelf175@gmail.com>
2014-08-14 07:52:17 -0400

    initial commit

---
+++ sudoku.rb
@@ -0,0 +1,53 @@
+
+puzzle = 
+    [5,3,4,6,7,8,9,1,2],
+    [6,7,2,1,9,5,3,4,8],
+    [1,9,8,3,4,2,5,6,7],
+
+    [8,5,9,7,6,1,4,2,3],
+    [4,2,6,8,5,3,7,9,1],
+    [7,1,3,9,2,4,8,5,6],
+
+    [9,6,1,5,3,7,2,8,4],
+    [2,8,7,4,1,9,6,3,5],
+    [3,4,5,2,8,6,1,7,9]
+
+
+array = []
+i = 0
+
+9.times do
+  total = 0
+
+  puzzle.each do |puz|
+    #p puz[i]
+    total += puz[i]
+    total
+  end
+  array << total
+  i += 1
+end
+
+p array ## checks columns
+
+
+
+array = []
+
+  puzzle.each do |puz|
+    total = 0  
+
+    total = puz.inject {|x, y| x + y }
+    array << total
+  end
+
+
+p array ## checks rows
+
+
+    
+
+
+
+
+
