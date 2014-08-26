f85ec8fd67427964a8855c12de1644cfd9768824
Ricky Bacci <brownieelf175@gmail.com>
2014-08-26 18:46:12 -0400

    refactor some tests

--- generate_puzzle.rb
+++ generate_puzzle.rb
@@ -16,6 +16,16 @@ require_relative 'print_puzzle'
 def gen2 box
 
 
+  def update_value num
+    if @puzzle[@array_element][@element_column] = 0
+      @puzzle[@array_element][@element_column] = num
+    end
+
+    @numbers_set << num
+    @numbers -= [num]
+  end
+
+
   def clear_array bx, num
     p "@puzzle[#{@array_element}][#{@element_column}] set to num: #{num}"
     #p "this is in test method with #{num}"
@@ -33,17 +43,33 @@ def gen2 box
     puts
     p "@box after all #{num}'s deleted"
     p bx
+    puts
   end
+  
 
+  def output_info
+    puts
+    p "numbers that were set this loop: #{@numbers_set}"
+
+    total_numbers_set = box0.select { |val| val != 0 }
+    p "total numbers in box that are set: #{total_numbers_set}"
+
+    p "numbers remaining to loop thru: #{@numbers - total_numbers_set}"
+    p "numbers remaining in box: #{@box}"
+    puts "@puzzle[#{@array_element}][#{@element_column}] -- End"
+    puts
+  end
 
   p "Starting box"
   p @box = box
   puts
+
   @array_element = 0
   @element_column = 0
   @numbers = [1,2,3,4,5,6,7,8,9]
   @i = 0
   @numbers_set = []
+
 
   @box.each do |element|
 
@@ -66,43 +92,38 @@ def gen2 box
           if ( @test.count(num) == 1 ) && ( element.include?(num) )
             #p "num: #{num}"
             p "number set because only 1: #{num} remaining and it exists in this element"
-            (@puzzle[@array_element][@element_column] = num) if (@puzzle[@array_element][@element_column] = 0)
-            update_puzzle
-
-            @numbers_set << num
-            @numbers -= [num]
             
-            #removes num from 'box'
-            clear_array(@box, num)
+            update_value num  # sets value in @puzzle
+            update_puzzle  #refreshes puzzle values
+            clear_array(@box, num)  #removes num from 'box'
+            output_info
+
             break ## needs to stay!
 
           elsif (element.length == 1) && ( element.include?(num) )#(element.empty? == false)
             #p "num: #{num}"
             p "element: #{element} set because element had '(array.length == 1) && (empty? == false)'"
-            (@puzzle[@array_element][@element_column] = num) if (@puzzle[@array_element][@element_column] = 0)
-            update_puzzle
-            @numbers_set << num
 
-            @numbers -= [num]
-
-            
-             #removes num from 'box'
-            clear_array(@box, num)
+            update_value num  # sets value in @puzzle
+            update_puzzle  #refreshes puzzle values
+            clear_array(@box, num)  #removes num from 'box'
+            output_info
 
             break ## needs to stay!
+
           else
             #p "NOTHING SET!"
           end
         else
           #p  "num doesn't exist in this element"
-
         end
       end
     else
-    p 'already set'
+      p "Value already set to: #{@puzzle[@array_element][@element_column]}"
     end
 
-    #puts "@puzzle[#{@array_element}][#{@element_column}] -- End"
+    puts "@puzzle[#{@array_element}][#{@element_column}] -- End"
+    puts
 
     if @element_column == 2
         @element_column = 0
@@ -114,16 +135,7 @@ def gen2 box
     else
       @element_column += 1
     end
-    puts
-    p "numbers that were set this loop: #{@numbers_set}"
-
-    total_numbers_set = box0.select { |val| val != 0 }
-    p "total numbers in box that are set: #{total_numbers_set}"
-
-    p "numbers remaining to loop thru: #{@numbers - total_numbers_set}"
-    p "numbers remaining in box: #{@box}"
-    puts "@puzzle[#{@array_element}][#{@element_column}] -- End"
-    puts
+    # output_info
     @numbers_set = []
     @i += 1
   end
