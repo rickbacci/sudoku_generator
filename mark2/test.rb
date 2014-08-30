def692e373503f5f48a2dcffb431800ba0e285d9
Ricky Bacci <brownieelf175@gmail.com>
2014-08-30 15:33:07 -0400

    attempt from different direction

---
+++ mark2/test.rb
@@ -0,0 +1,148 @@
+@new_puz = [
+  [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]], 
+  [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]], 
+  [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]],
+
+  [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]], 
+  [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]], 
+  [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]],
+
+  [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]], 
+  [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]], 
+  [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]]]
+
+#p 'boxes'
+#puts
+def box0
+ @box0 = @new_puz[0][0..2] + @new_puz[1][0..2] + @new_puz[2][0..2]
+end
+
+ @box1 = @new_puz[0][3..5] + @new_puz[1][3..5] + @new_puz[2][3..5]
+ @box2 = @new_puz[0][6..8] + @new_puz[1][6..8] + @new_puz[2][6..8]
+#puts
+ @box3 = @new_puz[3][0..2] + @new_puz[4][0..2] + @new_puz[5][0..2]
+ @box4 = @new_puz[3][3..5] + @new_puz[4][3..5] + @new_puz[5][3..5]
+ @box5 = @new_puz[3][6..8] + @new_puz[4][6..8] + @new_puz[5][6..8]
+#puts
+ @box6 = @new_puz[6][0..2] + @new_puz[7][0..2] + @new_puz[8][0..2]
+ @box7 = @new_puz[6][3..5] + @new_puz[7][3..5] + @new_puz[8][3..5]
+ @box8 = @new_puz[6][6..8] + @new_puz[7][6..8] + @new_puz[8][6..8]
+#puts
+#puts
+ #p 'columns'
+ # col0 = [] <<  @new_puz[0][0] << @new_puz[1][0] << @new_puz[2][0] << 
+ #               @new_puz[3][0] << @new_puz[4][0] << @new_puz[5][0] << 
+ #               @new_puz[6][0] << @new_puz[7][0] << @new_puz[8][0]
+
+ @col0 = @new_puz[0][0] + @new_puz[1][0] + @new_puz[2][0] + @new_puz[3][0] + @new_puz[4][0] + @new_puz[5][0] + @new_puz[6][0] + @new_puz[7][0] + @new_puz[8][0]
+ @col1 = @new_puz[0][1] + @new_puz[1][1] + @new_puz[2][1] + @new_puz[3][1] + @new_puz[4][1] + @new_puz[5][1] + @new_puz[6][1] + @new_puz[7][1] + @new_puz[8][1]
+ @col2 = @new_puz[0][2] + @new_puz[1][2] + @new_puz[2][2] + @new_puz[3][2] + @new_puz[4][2] + @new_puz[5][2] + @new_puz[6][2] + @new_puz[7][2] + @new_puz[8][2]
+#puts
+ @col3 = @new_puz[0][3] + @new_puz[1][3] + @new_puz[2][3] + @new_puz[3][3] + @new_puz[4][3] + @new_puz[5][3] + @new_puz[6][3] + @new_puz[7][3] + @new_puz[8][3]
+ @col4 = @new_puz[0][4] + @new_puz[1][4] + @new_puz[2][4] + @new_puz[3][4] + @new_puz[4][4] + @new_puz[5][4] + @new_puz[6][4] + @new_puz[7][4] + @new_puz[8][4]
+ @col5 = @new_puz[0][5] + @new_puz[1][5] + @new_puz[2][5] + @new_puz[3][5] + @new_puz[4][5] + @new_puz[5][5] + @new_puz[6][5] + @new_puz[7][5] + @new_puz[8][5]
+#puts
+ @col6 = @new_puz[0][6] + @new_puz[1][6] + @new_puz[2][6] + @new_puz[3][6] + @new_puz[4][6] + @new_puz[5][6] + @new_puz[6][6] + @new_puz[7][6] + @new_puz[8][6]
+ @col7 = @new_puz[0][7] + @new_puz[1][7] + @new_puz[2][7] + @new_puz[3][7] + @new_puz[4][7] + @new_puz[5][7] + @new_puz[6][7] + @new_puz[7][7] + @new_puz[8][7]
+ @col8 = @new_puz[0][8] + @new_puz[1][8] + @new_puz[2][8] + @new_puz[3][8] + @new_puz[4][8] + @new_puz[5][8] + @new_puz[6][8] + @new_puz[7][8] + @new_puz[8][8]
+
+#puts
+ 'rows'
+ @row0 = @new_puz[0]
+ @row1 = @new_puz[1]
+ @row2 = @new_puz[2]
+#puts
+ @row3 = @new_puz[3]
+ @row4 = @new_puz[4]
+ @row5 = @new_puz[5]
+#puts
+ @row6 = @new_puz[6]
+ @row7 = @new_puz[7]
+ @row8 = @new_puz[8]
+
+
+
+#p @new_puz[rand(0..8)][rand(0..8)] = [rand(1..9)]
+ @new_puz[0][0] = [1]
+
+p 'new_puz with 1 change'
+p @new_puz
+
+@puzzle_in_progress = @new_puz
+
+
+####---clears out rows
+@new_puz[0].each do |p|
+
+  if p.size == 1
+    #p "found 1 in row"
+     p @num = p[0]
+
+    ## clears row
+    @new_puz[0].each do |p|
+
+      unless p.size == 1
+        if p.include? (@num)
+          p.delete (@num)
+        end
+      end
+    end
+    #p "outside loop after deleting 1's in rows" 
+    #p @new_puz[0]
+    #puts
+  end
+end
+
+
+###---clears out columns
+@new_puz.each do |puz|
+  
+  #p 'column'
+  #p puz[0]
+  if puz[0].size == 1
+    p @num = puz[0][0]
+
+    #p 'inside if'
+    @new_puz.each do |p|
+      unless p[0].size == 1
+        #p 'inside unless'
+        #p p
+        #p p.size
+        
+        if p[0].include? (@num)
+          p[0].delete (@num)
+        end
+      end
+    end
+  end
+end
+
+box0
+p 'box0'
+###---clear box0
+@box0.each do |p|
+
+  if p.size == 1
+    p "found 1 in box0"
+     p @num = p[0]
+
+    ## clears box
+    @box0.each do |p|
+
+      unless p.size == 1
+        if p.include? (@num)
+          p.delete (@num)
+        end
+      end
+    end
+  end
+end
+
+
+@box0.each_slice(3) { |a| p a }
+puts
+
+@new_puz.each { |a| p a }
+
+
+
