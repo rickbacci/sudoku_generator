8e1d81fc2f2106b78bf3ba6fae5a10e6a2a30c7f
Ricky Bacci <brownieelf175@gmail.com>
2014-08-31 12:33:43 -0400

    refactor clear_columns

--- mark2/clear_columns.rb
+++ mark2/clear_columns.rb
@@ -1,51 +1,16 @@
-
 def clear_columns
 
-  columns = [@col0, @col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8]
+  (0..8).each do |col|
+    @new_puz.each do |puz|
+      if puz[col].size == 1
+        num = puz[col][0]
 
-  i = 0 # only for testing
-
-  columns.each do |col|
-    col.each do |c|
-
-      if c.size == 1
-
-        num = c[0]
-        #p "found a #{c[0]} in column #{i}"
-        col.each do |c|
-          unless c.size == 1
-            c.delete (num) if c.include? (num)
+        @new_puz.each do |p|
+          unless p[col].size == 1
+            p[col].delete (num) if p[col].include? (num)
           end
         end
       end
     end
-    i += 1 # only for testing
   end
 end
-
-
-
-
-
-  ###---clears out columns
-  # @new_puz.each do |puz|
-    
-  #   #p 'column'
-  #   #p puz[0]
-  #   if puz[0].size == 1
-  #     @num = puz[0][0]
-
-  #     #p 'inside if'
-  #     @new_puz.each do |p|
-  #       unless p[0].size == 1
-  #         #p 'inside unless'
-  #         #p p
-  #         #p p.size
-          
-  #         if p[0].include? (@num)
-  #           p[0].delete (@num)
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
