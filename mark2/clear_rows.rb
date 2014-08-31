24531df5689cf343b86f7dca5cea928057bef66c
Ricky Bacci <brownieelf175@gmail.com>
2014-08-31 14:38:29 -0400

    comment out print in clear_rows

--- mark2/clear_rows.rb
+++ mark2/clear_rows.rb
@@ -4,7 +4,7 @@ def clear_rows
     @new_puz[row_number].each do |row_val|
       if row_val.size == 1
         num = row_val[0]
-        p "found a #{num} in row #{row_number}"
+        #p "found a #{num} in row #{row_number}"
         @new_puz[row_number].each do |row_val|
           unless row_val.size == 1
             row_val.delete num if row_val.include? num
