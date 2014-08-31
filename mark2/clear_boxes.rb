81748554f3fcfd34af7d2f1b126a899c2784504d
Ricky Bacci <brownieelf175@gmail.com>
2014-08-31 14:36:27 -0400

    refactor clear_boxes

--- mark2/clear_boxes.rb
+++ mark2/clear_boxes.rb
@@ -1,22 +1,31 @@
 def clear_boxes
 
-  @boxes = [@box0, @box1, @box2, @box3, @box4, @box5, @box6, @box7, @box8]
-  i = 0 # testing only
+  box0 = @new_puz[0][0..2] + @new_puz[1][0..2] + @new_puz[2][0..2]
+  box1 = @new_puz[0][3..5] + @new_puz[1][3..5] + @new_puz[2][3..5]
+  box2 = @new_puz[0][6..8] + @new_puz[1][6..8] + @new_puz[2][6..8]
+  box3 = @new_puz[3][0..2] + @new_puz[4][0..2] + @new_puz[5][0..2]
+  box4 = @new_puz[3][3..5] + @new_puz[4][3..5] + @new_puz[5][3..5]
+  box5 = @new_puz[3][6..8] + @new_puz[4][6..8] + @new_puz[5][6..8]
+  box6 = @new_puz[6][0..2] + @new_puz[7][0..2] + @new_puz[8][0..2]
+  box7 = @new_puz[6][3..5] + @new_puz[7][3..5] + @new_puz[8][3..5]
+  box8 = @new_puz[6][6..8] + @new_puz[7][6..8] + @new_puz[8][6..8]
 
-  @boxes.each do |box|
-    box.each do |b| 
-      if b.size == 1
-        #p "found a #{b[0]} in box #{i}"
-        num = b[0]
 
-        box.each do |p|
-        
-          unless p.size == 1
-            p.delete (num) if p.include? (num)
+  boxes = [box0, box1, box2, box3, box4, box5, box6, box7, box8]
+  
+  (0..8).each do |box_number|
+    boxes.each do |box|
+      box.each do |b| 
+        if b.size == 1
+          num = b[0]
+          #p "found a #{num} in box #{box_number}"
+          box.each do |p|
+            unless p.size == 1
+              p.delete (num) if p.include? (num)
+            end
           end
         end
       end
     end
   end
-  i += 1 # testing only
 end
