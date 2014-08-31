6b83fbd17aadd16f536d0e0d08363ff2bc2b62c7
Ricky Bacci <brownieelf175@gmail.com>
2014-08-31 12:33:04 -0400

    refactor rows

--- mark2/clear_rows.rb
+++ mark2/clear_rows.rb
@@ -1,13 +1,13 @@
 
 def clear_rows
-  (0..8).each do |i|
-    @new_puz[i].each do |p|
+  (0..8).each do |row|
+    @new_puz[row].each do |p|
 
       if p.size == 1
         #p "found a #{p[0]} in row #{i}"
         num = p[0]
 
-        @new_puz[i].each do |p|
+        @new_puz[row].each do |p|
 
           unless p.size == 1
             p.delete num if p.include? num
