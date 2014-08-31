948fa2dce5ef487d613b460e2b2ce6e201b41620
Ricky Bacci <brownieelf175@gmail.com>
2014-08-31 12:31:50 -0400

    add file for box8

---
+++ mark2/set_box8.rb
@@ -0,0 +1,15 @@
+
+def set_box8
+  numbers = [[1],[2],[3],[4],[5],[6],[7],[8],[9]].shuffle!
+  shuffled_numbers = []
+  shuffled_numbers << @new_puz[6][6] = numbers.pop
+  shuffled_numbers << @new_puz[6][7] = numbers.pop
+  shuffled_numbers << @new_puz[6][8] = numbers.pop
+  shuffled_numbers << @new_puz[7][6] = numbers.pop
+  shuffled_numbers << @new_puz[7][7] = numbers.pop
+  shuffled_numbers << @new_puz[7][8] = numbers.pop
+  shuffled_numbers << @new_puz[8][6] = numbers.pop
+  shuffled_numbers << @new_puz[8][7] = numbers.pop
+  shuffled_numbers << @new_puz[8][8] = numbers.pop
+  shuffled_numbers
+end
