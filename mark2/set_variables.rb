dd033351cbc9fce8131a4433847e40586c9a0975
Ricky Bacci <brownieelf175@gmail.com>
2014-08-31 14:35:55 -0400

    comment out code in method

--- mark2/set_variables.rb
+++ mark2/set_variables.rb
@@ -1,29 +1,29 @@
 def set_variables
-  row0
-  row1
-  row2
-  row3
-  row4
-  row5
-  row6
-  row7
-  row8
-  col0
-  col1
-  col2
-  col3
-  col4
-  col5
-  col6
-  col7
-  col8
-  box0
-  box1
-  box2
-  box3
-  box4
-  box5
-  box6
-  box7
-  box8
+  # row0
+  # row1
+  # row2
+  # row3
+  # row4
+  # row5
+  # row6
+  # row7
+  # row8
+  # col0
+  # col1
+  # col2
+  # col3
+  # col4
+  # col5
+  # col6
+  # col7
+  # col8
+  # box0
+  # box1
+  # box2
+  # box3
+  # box4
+  # box5
+  # box6
+  # box7
+  # box8
 end
