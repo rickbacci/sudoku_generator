c0319e51c2ba6ed40cfb3175e3a06edc2e037db5
Ricky Bacci <brownieelf175@gmail.com>
2014-09-02 13:48:28 -0400

    solve for box working for array.size == 1 and 1 remaining value

--- mark2/clear_boxes.rb
+++ mark2/clear_boxes.rb
@@ -1,20 +1,20 @@
 def clear_boxes
 
-  box0 = @new_puz[0][0..2] + @new_puz[1][0..2] + @new_puz[2][0..2]
-  box1 = @new_puz[0][3..5] + @new_puz[1][3..5] + @new_puz[2][3..5]
-  box2 = @new_puz[0][6..8] + @new_puz[1][6..8] + @new_puz[2][6..8]
-  box3 = @new_puz[3][0..2] + @new_puz[4][0..2] + @new_puz[5][0..2]
-  box4 = @new_puz[3][3..5] + @new_puz[4][3..5] + @new_puz[5][3..5]
-  box5 = @new_puz[3][6..8] + @new_puz[4][6..8] + @new_puz[5][6..8]
-  box6 = @new_puz[6][0..2] + @new_puz[7][0..2] + @new_puz[8][0..2]
-  box7 = @new_puz[6][3..5] + @new_puz[7][3..5] + @new_puz[8][3..5]
-  box8 = @new_puz[6][6..8] + @new_puz[7][6..8] + @new_puz[8][6..8]
+  @box0 = @new_puz[0][0..2] + @new_puz[1][0..2] + @new_puz[2][0..2]
+  @box1 = @new_puz[0][3..5] + @new_puz[1][3..5] + @new_puz[2][3..5]
+  @box2 = @new_puz[0][6..8] + @new_puz[1][6..8] + @new_puz[2][6..8]
+  @box3 = @new_puz[3][0..2] + @new_puz[4][0..2] + @new_puz[5][0..2]
+  @box4 = @new_puz[3][3..5] + @new_puz[4][3..5] + @new_puz[5][3..5]
+  @box5 = @new_puz[3][6..8] + @new_puz[4][6..8] + @new_puz[5][6..8]
+  @box6 = @new_puz[6][0..2] + @new_puz[7][0..2] + @new_puz[8][0..2]
+  @box7 = @new_puz[6][3..5] + @new_puz[7][3..5] + @new_puz[8][3..5]
+  @box8 = @new_puz[6][6..8] + @new_puz[7][6..8] + @new_puz[8][6..8]
 
 
-  boxes = [box0, box1, box2, box3, box4, box5, box6, box7, box8]
+  @boxes = [@box0, @box1, @box2, @box3, @box4, @box5, @box6, @box7, @box8]
   
   (0..8).each do |box_number|
-    boxes.each do |box|
+    @boxes.each do |box|
       box.each do |b| 
         if b.is_a? Integer
           num = b
--- mark2/test.rb
+++ mark2/test.rb
@@ -8,6 +8,7 @@ require_relative 'set_box0'
 require_relative 'set_box4'
 require_relative 'set_box8'
 
+
 # set_box0
 # set_box4
 # set_box8
@@ -23,6 +24,7 @@ clear_all
 @loop_once = 0
 
 def solve_for_one_remaining_number_in_row ## works
+  @loop_once = 0
 
   (0..8).each do |row_number|
     column = 0
@@ -41,8 +43,9 @@ def solve_for_one_remaining_number_in_row ## works
 
       @new_puz[row_number].each_with_index do |row, index|
         unless row.is_a? Integer
-          if row.include? @one_num_remaining
+          if (row.include? @one_num_remaining) #&& @loop_once == 0
             @new_puz[row_number][index] = @one_num_remaining
+            @loop_once = 1
             return
           end
         end
@@ -51,41 +54,131 @@ def solve_for_one_remaining_number_in_row ## works
   end
 end
 
-
 def solve_for_one_remaining_number_in_column
+  @loop_once = 0
   build_column = []
+  build_column_flat = []
   @num_in_column_to_set = 0
 
-  @new_puz.each do |row|
-    unless row[0].is_a? Integer
-      build_column << row[0]
+  (0..8).each do |col|
+    @new_puz.each do |row|
+        build_column << row[col]
+
+      unless row[col].is_a? Integer
+        build_column_flat << row[col]
+      end
     end
-  end
 
-  flat = build_column.flatten
+    flat = build_column_flat.flatten
   
-  (1..9).each do |num|
-
-    if flat.count(num) == 1
-       @num_in_column_to_set = num
-    end 
-    
-    @new_puz.each_with_index do |row, index|
-      unless row[0].is_a? Integer
-         
-        if row[0].include? @num_in_column_to_set
-          @new_puz[index][0] = @num_in_column_to_set
-          #return
-        end 
+    (1..9).each do |num|
+      if flat.count(num) == 1
+         @num_in_column_to_set = num
+      end 
+      
+      @new_puz.each_with_index do |row, index|
+        unless row[col].is_a? Integer
+          if row[col].size == 1
+            p @new_puz[index][col] = @new_puz[index][col][0]
+            @loop_once = 1
+          elsif @loop_once == 0 && (row[col].include? @num_in_column_to_set)
+            @new_puz[index][col] = @num_in_column_to_set
+            @loop_once = 1
+          end 
+        end
       end
     end
   end
 end
 
-solve_for_one_remaining_number_in_row ## works
+
+def solve_for_box rows, columns
+  @loop_once = 0
+  box_array = []
+  @flat_array = []
+
+  rows.each do |row|
+    columns.each do |col|
+      box_array << @new_puz[row][col]
+      @flat_array << @new_puz[row][col] if @new_puz[row][col].is_a? Array
+    end
+  end
+  @flat_array = @flat_array.flatten
+
+  (1..9).each do |num|
+    rows.each do |row|
+      columns.each do |col|
+          #p @flat_array
+
+        element = @new_puz[row][col]
+        p @flat_array.count(num) == 1
+        p "row: #{row} column: #{col} element: #{@new_puz[row][col]}"
+        p "counting #{num}'s...there were #{@flat_array.count(num)} found"
+        if element.is_a? Array
+          if element.is_a?(Array) && (element.size == 1)
+            @new_puz[row][col] = @new_puz[row][col][0]
+            @loop_once = 1
+          elsif (@flat_array.count(num) == 1) && element.include?(num)
+            
+            p 'in here'
+            p num
+            @new_puz[row][col] = num if @loop_once == 0
+            @loop_once = 1
+          end
+        end
+      end
+    end
+  end
+
+
+
+
+end
+
+# solve_for_one_remaining_number_in_row ## works
+# clear_all
+
+# solve_for_one_remaining_number_in_column
+# clear_all
+
+# solve_for_one_remaining_number_in_column
+# clear_all
+
+# solve_for_one_remaining_number_in_row ## works
+# clear_all
+
+# solve_for_one_remaining_number_in_column
+# clear_all
+
+# # 2 in col1 row7
+# solve_for_one_remaining_number_in_column
+# clear_all
+
+# # [2] row5 col7 & [9] in row7 col5
+# solve_for_one_remaining_number_in_column
+# clear_all
+
+## row 3,4,5
+## col 0,1,2
+
+# should get the 2 in row4 col2
+p solve_for_box (3..5), (0..2)
 clear_all
-solve_for_one_remaining_number_in_column
+
+#should get the [3] in row3 col1
+p solve_for_box (3..5), (0..2)
 clear_all
+
+# row4 col0 solve for single 6
+p solve_for_box (3..5), (0..2)
+clear_all
+
+
+
+
+ 
+
+
 
 
 
