9e70b8bb1cdc32830a00f56992f4673034183164
Ricky Bacci <brownieelf175@gmail.com>
2014-09-07 00:03:50 -0400

    make methods with same structure in clear_all

--- mark2/clear_all.rb
+++ mark2/clear_all.rb
@@ -1,17 +1,11 @@
 
 def clear_all
   clear_rows
-  #clear_columns
-  #clear_boxes
+  clear_columns
+  clear_boxes
 end
 
-# def delete_values value, num
-#   value.each do |row_val|
-#     unless row_val.is_a? Integer
-#       row_val.delete num if row_val.include? num
-#     end
-#   end
-# end
+
 
 def clear_rows
 
@@ -19,34 +13,9 @@ def clear_rows
 
   @rows = [@row0, @row1, @row2, @row3, @row4, @row5, @row6, @row7, @row8]
 
-  
-  # (0..8).each do |row_number|
-  #   @rows.each do |row|
-
-  #     row.each do |r| 
-  #       p row
-  #       if r.is_a? Integer
-  #         num = r
-
-  #         delete_values row, num
-  #         # box.each do |p|
-  #         #   unless p.is_a? Integer
-  #         #     p.delete (num) if p.include? (num)
-  #         #   end
-  #         # end
-  #       end
-  #     end
-  #   end
-  # end
-
-
-
-
-
-
-  @rows.each_with_index do |row_number, index|
+  @rows.each do |row_number|
     row_number.each do |element|
-      p element
+
       if element.is_a?(Integer)
         number = element
 
@@ -58,19 +27,6 @@ def clear_rows
       end
     end
   end
-
-
-
-  # (0..8).each do |row|                                     
-  #   @new_puz[row].each do |row_val|                       
-  #     if row_val.is_a? Integer
-  #       num = row_val
-
-  #       delete_values @new_puz[row], num
-        
-  #     end
-  #   end
-  # end
 end
 
 
@@ -78,21 +34,17 @@ def clear_columns
 
   set_column_variables
 
-  columns = [@col0, @col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8]
+  @columns = [@col0, @col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8]
 
+  @columns.each do |col_number|
+    col_number.each do |element|
 
-  (0..8).each do |column|
-    column_array = []
-    @new_puz.each do |row|
-      column_array << row[column]
-    end
+      if element.is_a?(Integer)
+        number = element
 
-    @new_puz.each do |row|
-      if row[column].is_a? Integer
-        num = row[column]
-        @new_puz.each do |r|
-          unless r[column].is_a? Integer
-            r[column].delete (num) if r[column].include? (num)
+        col_number.each do |value|
+          unless value.is_a?(Integer)
+            value.delete(number) if value.include?(number)
           end
         end
       end
@@ -106,19 +58,18 @@ def clear_boxes
   set_box_variables
 
   @boxes = [@box0, @box1, @box2, @box3, @box4, @box5, @box6, @box7, @box8]
-  
-  (0..8).each do |box_number|
-    @boxes.each do |box|
-      box.each do |b| 
-        if b.is_a? Integer
-          num = b
 
-          delete_values box, num
-          # box.each do |p|
-          #   unless p.is_a? Integer
-          #     p.delete (num) if p.include? (num)
-          #   end
-          # end
+  @boxes.each do |box_number|
+
+    box_number.each do |element|
+
+      if element.is_a?(Integer)
+        number = element
+
+        box_number.each do |value|
+          unless value.is_a?(Integer)
+            value.delete(number) if value.include?(number)
+          end
         end
       end
     end
