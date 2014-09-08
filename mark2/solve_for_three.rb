5b45bf0b7620d723e7166473b6dab5d3678dc900
Ricky Bacci <brownieelf175@gmail.com>
2014-09-08 14:13:44 -0400

    refactor solve_for_three using number instead of first element

--- mark2/solve_for_three.rb
+++ mark2/solve_for_three.rb
@@ -5,9 +5,9 @@ def solve_for_three(arrays)
     arrays.each_with_index do |array, row|
       array.each_with_index do |element, column|
         if element.is_a?(Array) && @number_totals_remaining[ number - 1 ] != 1
-          if (element.size == 3) && @loop_once == 0
-
-            new_val = element - [element[0]]
+          if (element.size == 3) && ((@loop_once == 0) && element.include?(number)) 
+            
+            new_val = element - [number]
             arrays[row][column] = new_val
             @history << "puzzle[#{row}][#{column}] set from #{element} to #{new_val} in solve_for_three"
             
